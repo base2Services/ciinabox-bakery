78d986aec6dd05f9886998d16638375e202349b4
Gus Vine <a.vine@base2services.com>
2017-03-22 15:35:32 +1100

    download chef bundle if exists, else get cookbook tar

--- scripts/win2016/download_cookbooks.ps1
+++ scripts/win2016/download_cookbooks.ps1
@@ -14,9 +14,14 @@ try
   $CookbookVersion = $ENV:CB_BUILD_NO
   $ChefPath        = $ENV:CHEF_PATH
 
-  Write-Output "INFO: Downloading cookbooks from s3 location: $SourceBucket/$ChefPath/$CookbookVersion/cookbooks.tar.gz"
-  Read-S3Object -Region $BucketRegion -BucketName $SourceBucket -Key /$ChefPath/$CookbookVersion/cookbooks.tar.gz -File $GzipPath
-
+  try {
+    Write-Output "INFO: Downloading chef bundle from s3 location: $SourceBucket/$ChefPath/$CookbookVersion/chef-bundle.tar.gz"
+    Read-S3Object -Region $BucketRegion -BucketName $SourceBucket -Key /$ChefPath/$CookbookVersion/chef-bundle.tar.gz -File $GzipPath
+  } catch {
+    Write-Output "INFO: Bundle not found, downloading cookbooks from s3 location: $SourceBucket/$ChefPath/$CookbookVersion/cookbooks.tar.gz"
+    Read-S3Object -Region $BucketRegion -BucketName $SourceBucket -Key /$ChefPath/$CookbookVersion/cookbooks.tar.gz -File $GzipPath
+  }
+  
   Write-Output "INFO: Deleting dir $CookbookDir"
   if(Test-Path -Path $CookbookDir ){
     Remove-Item -Recurse -Force $CookbookDir
